a32cbdc6a2aa8452ffd42ca683f1f59614e76304
J F Kong <jfkong34@gmail.com>
2026-04-08 14:04:04 +0800

    changed exception type

--- src/qiboopt/__init__.py
+++ src/qiboopt/__init__.py
@@ -2,7 +2,7 @@ try:
     import importlib.metadata as im
 
     __version__ = im.version(__package__)
-except Exception:
+except im.PackageNotFoundError:
     __version__ = "0.0.1"
 
 from . import combinatorial, integrations, opt_class
